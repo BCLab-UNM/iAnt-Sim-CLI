4aef4954b7275a22b1d019341155c2781f0adfef
Neal Holtschulte <nealholtschulte@Neals-Mac.local>
2013-08-10 14:52:45 -0600

    Neal: I added extensive command line customization. Run from the command line with flags -h or -help to see the new available flags or just read the code in main.m

--- AntBot-GA-CLI/main.m
+++ AntBot-GA-CLI/main.m
@@ -21,6 +21,7 @@ int main(int argc, const char * argv[]) {
     [simulation setRobotCount:6];
     [simulation setTeamCount:100];
     [simulation setGenerationCount:100];
+    [simulation setEvaluationLimit:-1]; //Negative 1 makes simulation ignore this.
     [simulation setTagCount:256];
     [simulation setEvaluationCount:8];
     [simulation setExploreTime:0];
@@ -41,7 +42,182 @@ int main(int argc, const char * argv[]) {
     [simulation setMutationRate:0.1];
     [simulation setCrossoverOperator:UniformPointCrossId];
     [simulation setMutationOperator:FixedVarMutId];
-    [simulation setElitism:true];
+    [simulation setElitism:TRUE];
+    
+    //Read values from the command line
+    NSArray *args = [[NSProcessInfo processInfo] arguments];
+    if (args && ([args count] > 1)){
+        //Check if the user is requesting help.
+        if([args indexOfObject:[NSString stringWithFormat:@"-h"]] != -1 || [args indexOfObject:[NSString stringWithFormat:@"-help"]] != -1){
+            NSLog(@"%@",
+                  [NSString stringWithFormat:@"Optional command line flags with their default values are as follows:\n%@%d%@\n%@%d%@\n%@%d%@\n%@%d%@\n%@%f%@\n%@%f%@\n%@%f%@\n%@%f%@\n%@%f%@\n%@%d%@\n%@%d%@\n%@\n\n",
+                   @"-genLimit ",
+                   [simulation generationCount],
+                   @"   //Sets the generationCount, the limit on the number of generations. Set to -1 to ignore and use evaluationLimit instead.",
+                   @"-evalLimit ",
+                   [simulation evaluationLimit],
+                   @"   //Sets the evaluationLimit, the limit on the number of generations. Set to -1 to ignore and use generationCount instead.",
+                   @"-evalCount ",
+                   [simulation evaluationCount],
+                   @"   //Sets evaluationCount, the number of evaluations per individual.",
+                   @"-popSize ",
+                   [simulation teamCount],
+                   @"   //Sets the teamCount, aka population size.",
+                   @"-distribCluster ",
+                   [simulation distributionClustered],
+                   @"   //Sets the distributionClustered.",
+                   @"-distribPowerLaw ",
+                   [simulation distributionPowerlaw],
+                   @"   //Sets the distributionPowerLaw.",
+                   @"-distribRandom ",
+                   [simulation distributionRandom],
+                   @"   //Sets the distributionRandom.",
+                   @"-crossRate ",
+                   [simulation crossoverRate],
+                   @"   //Sets the crossover rate.",
+                   @"-mutRate ",
+                   [simulation mutationRate],
+                   @"   //Sets the mutation rate.",
+                   @"-mutOp ",
+                   [simulation mutationOperator],
+                   @"   //Sets the mutation operator.\n      //Mutation operator options include:\n      //0 for value-dependent variance mutation\n      //1 for decreasing variance mutation\n      //2 for fixed variance mutation",
+                   @"-crossOp ",
+                   [simulation crossoverOperator],
+                   @"   //Sets the crossover operator.\n      //Crossover operator options include:\n      //0 for independent assortment\n      //1 for uniform crossover\n      //2 for one point crossover\n      //3 for two point crossover",
+                   @"-noElitism   //Turns off elitism. Elitism is enabled by default."]);
+            exit(0);
+        }
+        //Set generationCount aka generationLimit
+        NSString *flag = [NSString stringWithFormat:@"-genLimit"];
+        int index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setGenerationCount:[[args objectAtIndex:(index+1)] intValue]];
+        }
+        //Set evaluationLimit
+        flag = [NSString stringWithFormat:@"-evalLimit"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setEvaluationLimit:[[args objectAtIndex:(index+1)] intValue]];
+        }
+        //Set evaluationCount. This is the number of evaluations per individual.
+        flag = [NSString stringWithFormat:@"-evalCount"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setEvaluationCount:[[args objectAtIndex:(index+1)] intValue]];
+        }
+        //Set teamCount aka population size.
+        flag = [NSString stringWithFormat:@"-popSize"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setTeamCount:[[args objectAtIndex:(index+1)] intValue]];
+        }
+        //Set distributionClustered
+        flag = [NSString stringWithFormat:@"-distribCluster"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setDistributionClustered:[[args objectAtIndex:(index+1)] floatValue]];
+        }
+        //Set distributionPowerlaw
+        flag = [NSString stringWithFormat:@"-distribPowerLaw"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setDistributionPowerlaw:[[args objectAtIndex:(index+1)] floatValue]];
+        }
+        //Set distributionRandom
+        flag = [NSString stringWithFormat:@"-distribRandom"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setDistributionRandom:[[args objectAtIndex:(index+1)] floatValue]];
+        }
+        //Set crossoverRate
+        flag = [NSString stringWithFormat:@"-crossRate"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setCrossoverRate:[[args objectAtIndex:(index+1)] floatValue]];
+        }
+        //Set mutationRate
+        flag = [NSString stringWithFormat:@"-mutRate"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setMutationRate:[[args objectAtIndex:(index+1)] floatValue]];
+        }
+        //Set mutation operator
+        flag = [NSString stringWithFormat:@"-mutOp"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            //This case statement is not strictly needed, but I want to use it for error checking and giving feedback to the user. See the default case.
+            switch ([[args objectAtIndex:(index+1)] intValue]){
+                case ValueDependentVarMutId:
+                    [simulation setMutationOperator:ValueDependentVarMutId];
+                    break;
+                case DecreasingVarMutId:
+                    [simulation setMutationOperator:DecreasingVarMutId];
+                    break;
+                case FixedVarMutId:
+                    [simulation setMutationOperator:FixedVarMutId];
+                    break;
+                default:
+                    NSLog(@"%@ is not a valid mutation operator. See help using -h for more information.", [[args objectAtIndex:(index+1)] stringValue]);
+                    exit(1);
+            }
+        }
+        //Set crossover operator
+        flag = [NSString stringWithFormat:@"-crossOp"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            //This case statement is not strictly needed, but I want to use it for error checking and giving feedback to the user. See the default case.
+            switch ([[args objectAtIndex:(index+1)] intValue]){
+                case IndependentAssortmentCrossId:
+                    [simulation setCrossoverOperator:IndependentAssortmentCrossId];
+                    break;
+                case UniformPointCrossId:
+                    [simulation setCrossoverOperator:UniformPointCrossId];
+                    break;
+                case OnePointCrossId:
+                    [simulation setCrossoverOperator:OnePointCrossId];
+                    break;
+                case TwoPointCross:
+                    [simulation setCrossoverOperator:TwoPointCross];
+                    break;
+                default:
+                    NSLog(@"%@ is not a valid crossover operator. See help using -h for more information.", [[args objectAtIndex:(index+1)] stringValue]);
+                    exit(1);
+            }
+        }
+        //Turn off elitism. Elitism is enabled by default.
+        flag = [NSString stringWithFormat:@"-noElitism"];
+        index = [args indexOfObject:flag];
+        if(index != -1){
+            [simulation setElitism:FALSE];
+        }
+    }
+    //print out the parameters to the console.
+    NSLog(@"%@",
+          [NSString stringWithFormat:@"%@%d\n%@%d\n%@%d\n%@%d\n%@%f\n%@%f\n%@%f\n%@%f\n%@%f\n%@%d\n%@%d\n%@%d\n",
+           @"generationCount aka generation limit = ",
+           [simulation generationCount],
+           @"evaluationLimit = ",
+           [simulation evaluationLimit],
+           @"evaluationCount = ",
+           [simulation evaluationCount],
+           @"teamCount aka population size = ",
+           [simulation teamCount],
+           @"distributionClustered = ",
+           [simulation distributionClustered],
+           @"distributionPowerlaw = ",
+           [simulation distributionPowerlaw],
+           @"distributionRandom = ",
+           [simulation distributionRandom],
+           @"crossoverRate = ",
+           [simulation crossoverRate],
+           @"mutationRate = ",
+           [simulation mutationRate],
+           @"mutationOperator = ",
+           [simulation mutationOperator],
+           @"crossoverOperator = ",
+           [simulation crossoverOperator],
+           @"elitism = ",
+           [simulation elitism]]);
     
     NSString *parameterFilePath = [NSString stringWithFormat:@"%@/evolvedParameters.plist",inputFilePath];
     if ([[NSFileManager defaultManager] fileExistsAtPath:parameterFilePath]) {
