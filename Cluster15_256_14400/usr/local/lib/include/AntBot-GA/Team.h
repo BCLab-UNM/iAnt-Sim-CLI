ab813ee5a0b860172ffcaf0c5ef64c58925935a9
Karl Stolleis <karl.stolleis@gmail.com>
2014-09-24 21:00:50 -0600

    initial Obstacle Class

---
+++ Cluster15_256_14400/usr/local/lib/include/AntBot-GA/Team.h
@@ -0,0 +1,38 @@
+#import <Foundation/Foundation.h>
+#import "Archivable.h"
+#import "Utilities.h"
+
+@interface Team : NSObject <Archivable> {}
+
+-(id) initRandom;
+-(id) initWithFile:(NSString*)filePath;
+
+//Behavior parameters:
+@property (nonatomic) float travelGiveUpProbability;
+@property (nonatomic) float searchGiveUpProbability;
+//@property (nonatomic) float decompositionAllocProbability;
+
+//Random walk parameters:
+@property (nonatomic) float uninformedSearchCorrelation;
+@property (nonatomic) float informedSearchCorrelationDecayRate;
+@property (nonatomic) float stepSizeVariation;
+
+//Information parameters:
+@property (nonatomic) float pheromoneDecayRate;
+@property (nonatomic) float pheromoneLayingRate;
+@property (nonatomic) float siteFidelityRate;
+
+//////////////POWER STUFF///////////////
+//@property (nonatomic) float powerReturnShift;
+//@property (nonatomic) float powerReturnSigma;
+//@property (nonatomic) float chargeActiveSigma;
+@property (nonatomic) float batteryReturnVal;       // The battery level at which the robot returns to charge
+@property (nonatomic) float batteryLeaveVal;        // The battery level at which the robot leaves during charging
+@property (nonatomic) float casualties;
+//////////////POWER STUFF///////////////
+
+//Non-evolved variables:
+@property (nonatomic) float fitness;
+@property (nonatomic) BOOL explorePhase; //Flag denoting whether robot team is currently exploring for tags instead of collecting them
+
+@end
---
+++ ClusterB_256_14400/usr/local/lib/include/AntBot-GA/Team.h
@@ -0,0 +1,29 @@
+#import <Foundation/Foundation.h>
+#import "Archivable.h"
+#import "Utilities.h"
+
+@interface Team : NSObject <Archivable> {}
+
+-(id) initRandom;
+-(id) initWithFile:(NSString*)filePath;
+
+//Behavior parameters:
+@property (nonatomic) float travelGiveUpProbability;
+@property (nonatomic) float searchGiveUpProbability;
+@property (nonatomic) float decompositionAllocProbability;
+
+//Random walk parameters:
+@property (nonatomic) float uninformedSearchCorrelation;
+@property (nonatomic) float informedSearchCorrelationDecayRate;
+@property (nonatomic) float stepSizeVariation;
+
+//Information parameters:
+@property (nonatomic) float pheromoneDecayRate;
+@property (nonatomic) float pheromoneLayingRate;
+@property (nonatomic) float siteFidelityRate;
+
+//Non-evolved variables:
+@property (nonatomic) float fitness;
+@property (nonatomic) BOOL explorePhase; //Flag denoting whether robot team is currently exploring for tags instead of collecting them
+
+@end
